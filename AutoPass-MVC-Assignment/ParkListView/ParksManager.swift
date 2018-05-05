786d76de56d4621dc8a6dce8a740ba65794bf1c9
廖慶麟 <hohey670@gmail.com>
2018-05-05 17:47:07 +0800

    implement ParksManager

---
+++ AutoPass-MVC-Assignment/ParkListView/ParksManager.swift
@@ -0,0 +1,42 @@
+//
+//  ParksManager.swift
+//  AutoPass-MVC-Assignment
+//
+//  Created by 廖慶麟 on 2018/5/5.
+//  Copyright © 2018年 廖慶麟. All rights reserved.
+//
+
+import Foundation
+
+enum UIState<T> {
+    case Loading
+    case Success([T])
+    case Failure
+}
+
+protocol ParksDelegate {
+    var state: UIState<Park> { get set }
+}
+
+protocol ParksHandler: class {
+    var delegate: ParksDelegate? { get set }
+    func loadData()
+}
+
+final class ParksManager: ParksHandler {
+    private let networkRequest: GetParkList
+    var delegate: ParksDelegate?
+    
+    init(networkRequest: GetParkList) {
+        self.networkRequest = networkRequest
+    }
+    
+    func loadData() {
+        guard let _ = self.delegate else { fatalError("Delegate needed") }
+        
+        delegate?.state = .Loading
+        networkRequest.perform().then { [weak self] (list) in
+            self?.delegate?.state = .Success(list.items)
+        }
+    }
+}
