56cbe170094883e33617f325a53e617166720763
廖慶麟 <hohey670@gmail.com>
2018-05-06 11:07:58 +0800

    replace constraints of ParkCell from SnapKit to Autolayout

--- AutoPass-MVC-Assignment/ParkListView/ParkCell.swift
+++ AutoPass-MVC-Assignment/ParkListView/ParkCell.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import SnapKit
 import SDWebImage
 
 extension ParkCell: CellConfigurable {}
@@ -26,72 +25,23 @@ final class ParkCell: UITableViewCell {
         }
     }
     
-    let mainContentView: UIView = UIView()
-    let mainImageView: UIImageView = UIImageView()
-    let nameLabel: UILabel = UILabel()
-    let adminAreaLabel: UILabel = UILabel()
-    let introLabel: UILabel = UILabel()
-    let starButton: UIButton = UIButton()
-    let mapButton: UIButton = UIButton()
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var adminAreaLabel: UILabel!
+    @IBOutlet weak var starButton: UIButton!
+    @IBOutlet weak var mapButton: UIButton!
+    @IBOutlet weak var introLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        addSubview(mainContentView)
-        mainContentView.layer.cornerRadius = 5.0
-        mainContentView.clipsToBounds = true
-        mainContentView.backgroundColor = UIColor.white
-        mainContentView.snp.makeConstraints { (make) in
-            make.left.right.equalToSuperview().inset(16)
-            make.top.bottom.equalToSuperview().inset(8)
-        }
-        
-        mainContentView.addSubview(mainImageView)
-        mainImageView.layer.cornerRadius = 22
+        mainImageView.layer.cornerRadius = mainImageView.frame.width/2
         mainImageView.clipsToBounds = true
-        mainImageView.snp.makeConstraints { (make) in
-            make.left.top.equalToSuperview().inset(4)
-            make.width.height.equalTo(44)
-        }
-        
-        mainContentView.addSubview(starButton)
         starButton.setTitle("Favorite", for: .normal)
-        starButton.snp.makeConstraints { (make) in
-            make.top.equalTo(mainImageView.snp.top)
-            make.trailing.equalToSuperview().inset(8)
-        }
-        
-        mainContentView.addSubview(mapButton)
         mapButton.setTitle("Map", for: .normal)
-        mapButton.snp.makeConstraints { (make) in
-            make.right.equalTo(starButton.snp.right)
-            make.top.equalTo(starButton.snp.bottom).offset(8)
-        }
-        
-        mainContentView.addSubview(nameLabel)
         nameLabel.font = UIFont.boldSystemFont(ofSize: 17)
-        nameLabel.snp.makeConstraints { (make) in
-            make.left.equalTo(mainImageView).inset(10)
-            make.right.equalTo(starButton).inset(10)
-            make.centerY.equalTo(starButton)
-        }
-        
-        mainContentView.addSubview(adminAreaLabel)
         adminAreaLabel.font = UIFont.systemFont(ofSize: 17)
-        adminAreaLabel.textColor = UIColor.gray
-        adminAreaLabel.snp.makeConstraints { (make) in
-            make.left.equalTo(nameLabel)
-            make.right.equalTo(mapButton).inset(10)
-            make.centerY.equalTo(mapButton)
-        }
-        
-        mainContentView.addSubview(introLabel)
         introLabel.font = UIFont.systemFont(ofSize: 14)
         introLabel.textColor = UIColor.lightGray
-        introLabel.numberOfLines = 5
-        introLabel.snp.makeConstraints { (make) in
-            make.left.right.equalToSuperview().inset(4)
-            make.top.equalTo(mainImageView.snp.bottom).offset(16)
-        }
     }
 }
